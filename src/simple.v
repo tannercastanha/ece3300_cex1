38ba44b41f43363d0404b637323d14c03ce1b503
tannercastanha <152110354+tannercastanha@users.noreply.github.com>
2026-09-14 17:37:24 -0700

    Fix output declaration and update result assignments

--- src/simple.v
+++ src/simple.v
@@ -19,18 +19,18 @@
 
 module simple(
 	      input [2:0]  a,
-	      output reg [6:0] result);
-
+	      output reg [6:0] result
+);
 	always @(a)
 		case(a)
-			3'b000: result = 0000000;
-			3'b001: result = 0000001;
-			3'b010: result = 0000011;
-			3'b011: result = 0000111;
-			3'b100: result = 0001111;
-			3'b101: result = 0011111;
-			3'b110: result = 0111111;
-			3'b111: result = 1111111;
+			3'b000: result = 7'b0000000;
+			3'b001: result = 7'b0000001;
+			3'b010: result = 7'b0000011;
+			3'b011: result = 7'b0000111;
+			3'b100: result = 7'b0001111;
+			3'b101: result = 7'b0011111;
+			3'b110: result = 7'b0111111;
+			3'b111: result = 7'b1111111;
 
 		end case
 endmodule
