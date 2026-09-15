d21dbda147e26ad3c876c31d8e0d4b5abf435385
tannercastanha <152110354+tannercastanha@users.noreply.github.com>
2026-09-14 17:38:22 -0700

    Fix case statement syntax in simple.v

--- src/simple.v
+++ src/simple.v
@@ -22,6 +22,7 @@ module simple(
 	      output reg [6:0] result
 );
 	always @(a)
+		begin
 		case(a)
 			3'b000: result = 7'b0000000;
 			3'b001: result = 7'b0000001;
@@ -32,7 +33,8 @@ module simple(
 			3'b110: result = 7'b0111111;
 			3'b111: result = 7'b1111111;
 
-		end case
+		end case;
+			end
 endmodule
 
 
